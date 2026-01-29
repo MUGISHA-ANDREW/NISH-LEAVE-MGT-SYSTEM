2e83e26654ee79d4269cf7b6878730c3dc2f5511
MUGISHA-ANDREW <andrewmugisha699@gmail.com>
2026-01-29 14:09:52 +0300

    production tests

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Nish Auto Limited')</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('./images/logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('./images/logo.jpg') }}">
     
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
