c853d4504781a5819b5a00974e97136b4d5c0c5f
MUGISHA-ANDREW <andrewmugisha699@gmail.com>
2026-02-19 15:12:36 +0300

    email notifications

--- config/mail.php
+++ config/mail.php
@@ -50,6 +50,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'gmail' => [
+            'transport' => 'gmail',
+        ],
+
 
 
         'ses' => [
